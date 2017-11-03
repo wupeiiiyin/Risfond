3a93c5853f10060b8fe3f0a6ccb97c5c38fcaab9
liuyunlong <2509881932@qq.com>
2017-11-03 14:42:44 +0800

    测试

--- app/src/main/java/com/risfond/rnss/application/MyApplication.java
+++ app/src/main/java/com/risfond/rnss/application/MyApplication.java
@@ -36,7 +36,6 @@ public class MyApplication extends Application {
 
             @Override
             public void onViewInitFinished(boolean arg0) {
-                // TODO Auto-generated method stub
                 //x5內核初始化完成的回调，为true表示x5内核加载成功，否则表示x5内核加载失败，会自动切换到系统内核。
             }
 
